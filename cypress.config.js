e0b68dfd40b166a576de79d25c22d7e856b29526
NaLeum <wognsdl@likelion.org>
2023-06-07 11:00:13 +0900

    feat: make ci

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: "himvmq",
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(_on, _config) {
+      // ...
+    },
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+};
