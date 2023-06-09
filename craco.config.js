6aafb5c1741b2420c27b48d6460251472d63ecb2
NaLeum <wognsdl@likelion.org>
2023-06-09 15:49:10 +0900

    feat: babel 설정

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,17 @@ const cracoEsbuildPlugin = require('craco-esbuild');
 
 module.exports = {
   plugins: [{ plugin: cracoEsbuildPlugin }],
-  webpack: smp.wrap({}),
+  webpack: smp.wrap({
+    configure: webpackConfig => {
+      webpackConfig.module.rules.push({
+        test: /\.(js|mjs|jsx)$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      });
+
+      return webpackConfig;
+    },
+  }),
 };
